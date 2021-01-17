08e6f1a1af2da709ae95e0167e3c4879149931bd
Regis Lima Claus <regis.claus@gmail.com>
2021-01-17 17:28:10 -0300

    Busca de Trampo iniciada

--- Partiu/Assets/_project/Scripts/HomeScript.cs
+++ Partiu/Assets/_project/Scripts/HomeScript.cs
@@ -6,16 +6,43 @@ public class HomeScript : MonoBehaviour
 {
 
     [SerializeField] private GameObject trilhas;
-
-    // Start is called before the first frame update
+    [SerializeField] private GameObject trampos;
+    [SerializeField] private GameObject comunidades;
+    [SerializeField] private GameObject chatTrampo;
+    
     void Start()
     {
         trilhas.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    public void Trilhas() {
+        trilhas.SetActive(true);
+        trampos.SetActive(false);
+        comunidades.SetActive(false);
+        chatTrampo.SetActive(false);
     }
+
+    public void Trampos() {
+        trilhas.SetActive(false);
+        trampos.SetActive(true);
+        comunidades.SetActive(false);
+        chatTrampo.SetActive(false);
+    }
+
+    public void Comunidades() {
+        trilhas.SetActive(false);
+        trampos.SetActive(false);
+        comunidades.SetActive(true);
+        chatTrampo.SetActive(false);
+    }
+
+    public void ChatTrampo() {
+        trilhas.SetActive(false);
+        trampos.SetActive(false);
+        comunidades.SetActive(false);
+        chatTrampo.SetActive(true);
+    }
+
+
 }
