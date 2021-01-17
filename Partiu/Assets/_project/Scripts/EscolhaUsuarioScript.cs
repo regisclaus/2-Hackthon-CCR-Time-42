118dd7380d377554e00e42fcfd16ddac729eece4
Regis Lima Claus <regis.claus@gmail.com>
2021-01-17 03:22:00 -0300

    Tela de Escolha de Usuario layout arrumado

---
+++ Partiu/Assets/_project/Scripts/EscolhaUsuarioScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class EscolhaUsuarioScript : MonoBehaviour
+{
+
+    private Usuario usuario = Usuario.JOVEM;
+
+    [SerializeField] private GameObject jovemTela;
+
+    [SerializeField] private GameObject empresarioTela;
+
+    [SerializeField] private DOTweenAnimation imagemJovem;
+
+    [SerializeField] private DOTweenAnimation imagemEmpresario;
+
+    public void AnimaJovem() {
+        imagemJovem.gameObject.SetActive(true);
+        imagemJovem.DORestart();
+        imagemEmpresario.gameObject.SetActive(false);
+        usuario = Usuario.JOVEM;
+    }
+
+    public void AnimaEmpresario() {
+        imagemEmpresario.gameObject.SetActive(true);
+        imagemEmpresario.DORestart();
+        imagemJovem.gameObject.SetActive(false);
+        usuario = Usuario.EMPRESARIO;
+    }
+
+    public void Entrar() {
+        switch(usuario) {
+            case Usuario.JOVEM:
+            GetComponent<ScreenScript>().GoToNextScreen(jovemTela);
+            break;
+            case Usuario.EMPRESARIO:
+            GetComponent<ScreenScript>().GoToNextScreen(empresarioTela);
+            break;
+        }
+    }
+}
+
+public enum Usuario { JOVEM, EMPRESARIO }
