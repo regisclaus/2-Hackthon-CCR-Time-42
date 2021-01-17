63513d125ae7da39700cf3b6d4b089766414ca2c
Regis Lima Claus <regis.claus@gmail.com>
2021-01-17 17:11:42 -0300

    Trilhas e Busca de Trampo

---
+++ Partiu/Assets/_project/Scripts/TrilhasScripts.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrilhasScripts : MonoBehaviour
+{
+
+    [SerializeField] private GameObject minhaTrilha;
+    [SerializeField] private GameObject trilhaTrampos;
+    [SerializeField] private GameObject trilhaPegada;
+    [SerializeField] private GameObject trilhaCabeca;
+
+    public void MinhaTrilha() {
+        minhaTrilha.SetActive(true);
+        trilhaTrampos.SetActive(false);
+        trilhaPegada.SetActive(false);
+        trilhaCabeca.SetActive(false);
+    }
+
+    public void TrilhaTrampos() {
+        minhaTrilha.SetActive(false);
+        trilhaTrampos.SetActive(true);
+        trilhaPegada.SetActive(false);
+        trilhaCabeca.SetActive(false);
+    }
+
+    public void TrilhaPegada() {
+        minhaTrilha.SetActive(false);
+        trilhaTrampos.SetActive(false);
+        trilhaPegada.SetActive(true);
+        trilhaCabeca.SetActive(false);
+    }
+
+    public void TrilhaCabeca() {
+        minhaTrilha.SetActive(false);
+        trilhaTrampos.SetActive(false);
+        trilhaPegada.SetActive(false);
+        trilhaCabeca.SetActive(true);
+    }
+}
