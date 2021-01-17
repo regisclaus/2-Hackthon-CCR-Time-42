0cee9eb63f3ac287f16da27b9f51c74b6a9cf57f
Regis Lima Claus <regis.claus@gmail.com>
2021-01-17 15:21:46 -0300

    Inicio de trilhas com cursos

---
+++ Partiu/Assets/_project/Scripts/AtividadeVideoScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class AtividadeVideoScript : MonoBehaviour
+{
+
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    private void Start() {
+        videoPlayer.time = 15f;
+    }
+
+    public void PlayVideo() {
+        videoPlayer.Play();
+    }
+
+    public void PauseVideo() {
+        videoPlayer.Pause();
+    }
+}
---
+++ Partiu/Assets/_project/Scripts/HomeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject trilhas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trilhas.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
