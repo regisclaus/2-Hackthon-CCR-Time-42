89d3553b9efb0f0c67c77f422a552d11949482b6
Regis Lima Claus <regis.claus@gmail.com>
2021-01-16 23:08:54 -0300

    Telas 1 2 3 4

---
+++ Partiu/Assets/_project/Scripts/ScreenScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using DG.Tweening;
+
+public class ScreenScript : MonoBehaviour
+{
+
+    [SerializeField] public ExitAnimationMoveSide exitAnimationMoveSide = ExitAnimationMoveSide.LEFT;
+    [SerializeField] public ScreenScript parentScreen;
+
+    // [SerializeField] public ScreenScript previosScreen;
+
+    [Header("Internal References")]
+    public DOTweenAnimation doTweenAnimation;
+
+    private void Awake() {
+        doTweenAnimation = GetComponent<DOTweenAnimation>();
+    }
+
+    public void GoToNextScreen(GameObject nextScreen) {
+        nextScreen.SetActive(true);
+        parentScreen?.ExitAnimation();
+        ExitAnimation();
+    }
+
+    public void GoToPreviousScreen(ScreenScript previousScreen) {
+        previousScreen.EnterAnimation();
+    }
+    
+    private void ExitAnimation() {
+        switch(exitAnimationMoveSide) {
+            case ExitAnimationMoveSide.LEFT:
+                doTweenAnimation.DOPlayById("LeftExit");
+                break;
+            case ExitAnimationMoveSide.DOWN:
+                doTweenAnimation.DOPlayById("DownExit");
+                break;
+            case ExitAnimationMoveSide.NONE:
+                break;
+        }
+    }
+
+    private void EnterAnimation() {
+        switch(exitAnimationMoveSide) {
+            case ExitAnimationMoveSide.LEFT:
+                doTweenAnimation.DOPlayById("LeftExit");
+                break;
+            case ExitAnimationMoveSide.DOWN:
+                doTweenAnimation.DOPlayById("DownExit");
+                break;
+            case ExitAnimationMoveSide.NONE:
+                break;
+        }
+    }
+
+}
+
+[Serializable]
+public enum ExitAnimationMoveSide {
+    LEFT,
+    DOWN,
+    NONE
+}
---
+++ Partiu/Assets/_project/Scripts/StartScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ScreenScript))]
+public class StartScreen : MonoBehaviour
+{
+    [SerializeField] public GameObject nextScreen;
+    [SerializeField] private float timeNextScreen = 2.0f;
+
+    void Start()
+    {
+        Invoke("GoToNextScreen", timeNextScreen);
+    }
+
+    private void GoToNextScreen() {
+        GetComponent<ScreenScript>().GoToNextScreen(nextScreen);
+    }
+}
